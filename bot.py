352a1d006520113808b18d95dd6b127b4485bac3
Mikita Iwanowski <info@slnt-opp.xyz>
2020-10-13 16:40:42 +0200

    bot v1

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
+from morse import MorseCodeTranslator
 from uuid import uuid4
 
 from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent
@@ -12,12 +12,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
-logger = logging.getLogger(__name__)
+morse_code_translator = MorseCodeTranslator()
 
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -32,18 +27,36 @@ def help_command(update, context):
     update.message.reply_text('Help!')
 
 
+def make_description(text):
+    return text[:16] + '...'
+
+
 def inlinequery(update, context):
     """Handle the inline query."""
     query = update.inline_query.query
-    if "m/" in query:
+    if not query:
         results = [
             InlineQueryResultArticle(
-                id=uuid4(), title="Result", description="words...", input_message_content=InputTextMessageContent("words words words"), thumb_url="https://i.pinimg.com/originals/f7/86/44/f7864403f01d75ac2d24944ac836ae1f.png")
+                id=uuid4(), title="Waiting for input", description="Begin entering your message or begin with m/ to decode morse code",
+                input_message_content=InputTextMessageContent(
+                    "Hungry for Morse?"),
+                thumb_url="https://dragonflytraining.files.wordpress.com/2013/10/man-with-question-01.png?w=600&h=600")
+        ]
+    elif "m/" in query[:2]:
+        r = morse_code_translator.translate_morse(query[2:])
+        results = [
+            InlineQueryResultArticle(
+                id=uuid4(), title="Result", description=make_description(r),
+                input_message_content=InputTextMessageContent(r),
+                thumb_url="https://i.pinimg.com/originals/f7/86/44/f7864403f01d75ac2d24944ac836ae1f.png")
         ]
     else:
+        r = morse_code_translator.translate_text(query)
         results = [
             InlineQueryResultArticle(
-                id=uuid4(), title="Morse code", description=".-.. .-- -.-", input_message_content=InputTextMessageContent(".-.. .-- -.-"), thumb_url="https://camo.githubusercontent.com/3494de09199b6b16fce39dfce8b20ccd1b2f8be0/687474703a2f2f7777772e636f64656275672e6f72672e756b2f6173736574732f73746570732f3534302f696d6167655f312e706e67")
+                id=uuid4(), title="Morse code", description=make_description(r),
+                input_message_content=InputTextMessageContent(r),
+                thumb_url="https://camo.githubusercontent.com/3494de09199b6b16fce39dfce8b20ccd1b2f8be0/687474703a2f2f7777772e636f64656275672e6f72672e756b2f6173736574732f73746570732f3534302f696d6167655f312e706e67")
         ]
 
     update.inline_query.answer(results)
